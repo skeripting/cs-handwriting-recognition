36cddff16401470a8c683ddb8c3aa9b94c8a0f47
skeripting <scripting1337@gmail.com>
2025-05-02 12:57:27 -0400

    mostly done. just getting random program crashes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +22,58 @@ namespace HandwritingRecognition
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public static string GetPrediction(string imagePath)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "python", 
+                Arguments = $"\"C:\\Users\\kusha\\source\\repos\\HandwritingRecognition\\digit-recognizer\\main.py\" \"{imagePath}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (var process = Process.Start(psi))
+            {
+                if (process == null)
+                {
+                    MessageBox.Show("Couldn't find the handwriting recognizer process!");
+                    return "ERROR"; 
+                }
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    return $"Error: {error}";
+                }
+
+                return output.Trim(); 
+            }
+        }
+
+        private void OnUploadClicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.PNG;*.JPG;*.BMP;*.GIF;)|*.PNG;*.JPG;*.BMP;*.GIF;|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+
+                imagePreview.Source = new BitmapImage(new Uri(filePath));
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                string prediction = GetPrediction(filePath);
+
+                predictedDigitLabel.Content = prediction;
+            }
         }
     }
 }
